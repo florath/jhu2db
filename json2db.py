32e1caea5ff2910c1879ff9d40cb92b037d0882e
Andreas Florath <andreas.florath@telekom.de>
2020-03-21 09:08:07 +0100

    Added small tool to push the json data into a mongodb
    Signed-off-by: Andreas Florath <andreas.florath@telekom.de>

---
+++ json2db.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import pymongo
+import json
+import os
+
+def mongodb_connect():
+    password = os.environ['BENE_MONGODB_PASSWORD']
+    client = pymongo.MongoClient("mongodb://root:%s@bene.gridpiloten.de:27017/" % password)
+    db = client["jhu"]
+    return db["cases"]
+
+def read_data():
+    with open("data/jhu-data-20200321-0830.json") as fd:
+        jdata = json.load(fd)
+    return jdata
+
+def main():
+    col = mongodb_connect()
+    data = read_data()
+    col.insert_many(data)
+
+if __name__ == '__main__':
+    main()
